b5a759a581268bb1bdfba25a5eaa42dcae99f79b
Zbcolgrim <Zachary.n.buffington@gmail.com>
2022-10-01 15:52:18 -0600

    First commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Zachary Buffington on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

10d2eaea7325738df4b9e23486e5c0e0e2066262
Zbcolgrim <Zachary.n.buffington@gmail.com>
2022-10-08 20:02:32 -0600

    CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
+        
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape =  RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
+        } else {
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+        }
     }
 }
 
@@ -18,4 +42,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
